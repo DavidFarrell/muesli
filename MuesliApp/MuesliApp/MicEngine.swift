05b77338ba004c97b1af775619544d4d87d728b2
David Gérouville-Farrell <davidfarrell81@gmail.com>
2026-09-06 00:27:20 +0100

    Preserve Sendable contract for stored microphone route callback

--- MuesliApp/MuesliApp/MicEngine.swift
+++ MuesliApp/MuesliApp/MicEngine.swift
@@ -13,7 +13,7 @@ actor MicEngine: MicCapturing {
     private var engine: AVAudioEngine?
     private var isRunning = false
     private var processor: MicCaptureProcessor?
-    private var onConfigurationChange: (() -> Void)?
+    private var onConfigurationChange: (@Sendable () -> Void)?
     private var configChangeObserver: NSObjectProtocol?
     private var retiredEngines: [UUID: AVAudioEngine] = [:]
     private let engineRetainDurationNs: UInt64 = 2_000_000_000
